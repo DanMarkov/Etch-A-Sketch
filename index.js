3371c520f93c86217da77964e1bb0ac3217c6fe4
Kenny <89842110+DanMarkov@users.noreply.github.com>
2024-02-20 08:29:03 +0800

    feat: make canvas creation with user's input
    A user needs to create a canvas, so they click on the button
    and change pixel ratio to their needs. After that a new canvas
    is created.

--- index.js
+++ index.js
@@ -3,25 +3,70 @@ container.classList.add("container");
 
 document.body.append(container);
 
-let divs = [];
+let button = document.createElement("button");
+button.textContent = "CLICK ME!";
+document.body.insertBefore(button, container);
 
-for (let i = 1; i < 5; i++) {
-    divs[i] = document.createElement('div');
-    divs[i].classList.add("grid");
-    divs[i].classList.add(`row-${[i]}`);
-    divs[i].textContent = `Contents of ${[i]} row`;
-    container.appendChild(divs[i]);
+button.addEventListener("click", () => {
+    textContent = +prompt("Create a new canvas", 64);
 
-    container.addEventListener("mouseover", (e) => {
+    grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.classList.add("row");
+
+    if (textContent === 0) {
+        exit;
+    } else if (!Number(textContent)) {
+        alert("Enter number, please...");
+    } else if (textContent > 100) {
+        alert("Your number is too high");
+    } else {
+        grid.textContent = `${textContent}`;
+        grid.style.width = `${textContent}px`;
+        grid.style.height = `${textContent}px`;
+        grid.style.lineHeight = `${textContent}px`;
+        container.appendChild(grid);
+    }
+
+    
+    if (document.querySelectorAll(".grid").length > 1) {
+        container.removeChild(container.firstElementChild);
+    }
+
+    grid.addEventListener("mouseenter", (e) => {
         
         e.target.style.backgroundColor = "chocolate";
 
     }, false);
 
-    container.addEventListener("mouseout", (e) => {
+    grid.addEventListener("mouseleave", (e) => {
 
         e.target.style.backgroundColor = "";
 
     }, false);
-}
+});
+
+
+
+// let divs = [];
+
+// for (let i = 1; i < 5; i++) {
+//     divs[i] = document.createElement('div');
+//     divs[i].classList.add("grid");
+//     divs[i].classList.add(`row-${[i]}`);
+//     divs[i].textContent = `Contents of ${[i]} row`;
+//     container.appendChild(divs[i]);
+
+    // container.addEventListener("mouseover", (e) => {
+        
+    //     e.target.style.backgroundColor = "chocolate";
+
+    // }, false);
+
+    // container.addEventListener("mouseout", (e) => {
+
+    //     e.target.style.backgroundColor = "";
+
+    // }, false);
+// }
 
