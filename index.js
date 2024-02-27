70a8704b976571c82237ba1a69716c3496ae771d
Kenny <89842110+DanMarkov@users.noreply.github.com>
2024-02-27 08:13:52 +0800

    feat: make hover effect over squares
    With this commit color of squares is changed
    when mouse hovers over them

--- index.js
+++ index.js
@@ -1,85 +1,180 @@
-let container = document.createElement('div');
-container.classList.add("container");
+document.addEventListener('DOMContentLoaded', () => {
 
-document.body.append(container);
+  let interface = document.createElement('div');
+  interface.classList.add("interface");
 
-let button = document.createElement("button");
-button.textContent = "CLICK ME!";
-document.body.insertBefore(button, container);
+  document.body.append(interface);
 
-function randomColor() {
-    let r = Math.floor(Math.random() * 255) + 1;
-    let g = Math.floor(Math.random() * 255) + 1;
-    let b = Math.floor(Math.random() * 255) + 1;
-    return rgb = "rgb(" + r + " " + g + " " + b + ")";
+  let settings = document.createElement('div');
+  settings.classList.add("settings");
+  interface.appendChild(settings);
+
+  let clearBtn = document.createElement("button");
+  clearBtn.textContent = "Clear Grid";
+  clearBtn.classList.add("clear");
+  clearBtn.classList.add("btn");
+  settings.appendChild(clearBtn);
+
+  let rainbowBtn = document.createElement("button");
+  rainbowBtn.textContent = "Rainbow Colour";
+  rainbowBtn.classList.add("rainbow");
+  rainbowBtn.classList.add("btn");
+  settings.appendChild(rainbowBtn);
+
+  let shadeBtn = document.createElement("button");
+  shadeBtn.textContent = "Shading";
+  shadeBtn.classList.add("shade");
+  shadeBtn.classList.add("btn");
+  settings.appendChild(shadeBtn);
+
+  let container = document.createElement("div");
+  container.classList.add("container");
+  settings.appendChild(container);
+
+  function createGrid(size) {
+    container.innerHTML = "";
+    for (i = 0; i < size * size; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      container.appendChild(square);
+    }
+    container.style.width = `${size * 60}px`;
+    addMouseoverToGrids();
+  }
+
+
+  shadeBtn.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+       square.addEventListener('mouseover', darkenPencilColor());
+
+        });
+  });
+
+
+function addMouseoverToGrids() {
+  const squares = document.querySelectorAll('.square');
+  if (rainbowColorIsOn == true) {
+      rainbowBtn.style.backgroundColor = "LightGreen";
+      squares.forEach(square => square.addEventListener('mouseover', changePencilColor(randomColor())));
+  } else {
+    rainbowBtn.style.backgroundColor = "";
+      squares.forEach(square => square.addEventListener('mouseover', changePencilColor('rgb(0, 0, 0)')));
+  }
 }
 
+function changePencilColor(color) {
+  return function(e) {
+      e.target.style.backgroundColor = color;
+  }
+}
 
+  let rainbowColorIsOn = false;
 
-button.addEventListener("click", () => {
-    textContent = +prompt("Create a new canvas", 64);
+  rainbowBtn.addEventListener('click', () => {
+    rainbowColorIsOn = !rainbowColorIsOn
+    addMouseoverToGrids();
+  });
 
-    grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.classList.add("row");
+  shadeBtn.addEventListener('click', darkenPencilColor);
 
-    if (textContent === 0) {
-        exit;
-    } else if (!Number(textContent)) {
-        alert("Enter number, please...");
-    } else if (textContent > 100) {
-        alert("Your number is too high");
-    } else {
-        grid.style.backgroundColor = randomColor();
-        grid.textContent = `${textContent}`;
-        grid.style.width = `${textContent}px`;
-        grid.style.height = `${textContent}px`;
-        grid.style.lineHeight = `${textContent}px`;
-        container.appendChild(grid);
+  function darkenPencilColor() {
+    let dark = 255;
+    return function(e) {
+        dark -= 25;
+        e.target.style.backgroundColor = `rgb(${dark}, ${dark}, ${dark})`;
     }
+  }
 
+
+
+  clearBtn.addEventListener("click", () => {
+    const newSize = +prompt("Create a new canvas", 16);
+    const size = Math.min(Math.max(parseInt(newSize) || 16, 1), 100);
+    createGrid(size);
+  });
+
+  createGrid(16);
+
+
+  function randomColor() {
+      let r = Math.floor(Math.random() * 255) + 1;
+      let g = Math.floor(Math.random() * 255) + 1;
+      let b = Math.floor(Math.random() * 255) + 1;
+      return rgb = "rgb(" + r + " " + g + " " + b + ")";
+  }
+
+
+
+  // dots is an array of Dot objects,
+  // mouse is an object used to track the X and Y position
+    // of the mouse, set with a mousemove event listener below
+    let dots = [],
+      mouse = {
+        x: 0,
+        y: 0
+      };
+
+  // The Dot object used to scaffold the dots
+  let Dot = function() {
+    this.x = 0;
+    this.y = 0;
+    this.node = (function(){
+      let node = document.createElement("div");
+      node.className = "trail";
+      node.style.backgroundColor = randomColor();
+      document.body.appendChild(node);
+      return node;
+    }());
+  };
+  // The Dot.prototype.draw() method sets the position of 
+    // the object's <div> node
+  Dot.prototype.draw = function() {
+    this.node.style.left = this.x + "px";
+    this.node.style.top = this.y + "px";
+  };
+
+  // Creates the Dot objects, populates the dots array
+  for (let i = 0; i < 50; i++) {
+    let d = new Dot();
+    dots.push(d);
+  }
+
+  // This is the screen redraw function
+  function draw() {
+    // Make sure the mouse position is set everytime
+      // draw() is called.
+    let x = mouse.x,
+        y = mouse.y;
     
-    if (document.querySelectorAll(".grid").length > 1) {
-        container.removeChild(container.firstElementChild);
-    }
+    // This loop is where all the 90s magic happens
+    dots.forEach(function(dot, index, dots) {
+      let nextDot = dots[index + 1] || dots[0];
+      
+      dot.x = x;
+      dot.y = y;
+      dot.draw();
+      x += (nextDot.x - dot.x) * .6;
+      y += (nextDot.y - dot.y) * .6;
 
+    });
+  }
 
-    grid.addEventListener("mouseenter", (e) => {
-        
-        e.target.style.backgroundColor = randomColor();
+  addEventListener("mousemove", function(event) {
+  //   event.preventDefault();
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+  });
 
-    }, false);
+  // animate() calls draw() then recursively calls itself
+    // everytime the screen repaints via requestAnimationFrame().
+  function animate() {
+    draw();
+    requestAnimationFrame(animate);
+  }
 
-    grid.addEventListener("mouseleave", (e) => {
+  // And get it started by calling animate().
+  animate();
 
-        e.target.style.backgroundColor = randomColor();
-
-    }, false);
-
-    console.log(randomColor())
 });
-
-
-
-// let divs = [];
-
-// for (let i = 1; i < 5; i++) {
-//     divs[i] = document.createElement('div');
-//     divs[i].classList.add("grid");
-//     divs[i].classList.add(`row-${[i]}`);
-//     divs[i].textContent = `Contents of ${[i]} row`;
-//     container.appendChild(divs[i]);
-
-    // container.addEventListener("mouseover", (e) => {
-        
-    //     e.target.style.backgroundColor = "chocolate";
-
-    // }, false);
-
-    // container.addEventListener("mouseout", (e) => {
-
-    //     e.target.style.backgroundColor = "";
-
-    // }, false);
-// }
 
